ebbb5fffce623e047b14b448aa970d9dc9c0b027
Shivan Sagar Gupta <guptajisagar414@gmail.com>
2026-01-08 15:30:25 +0530

    df

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,76 @@
-import {BrowserRouter as Router ,Routes ,Route} from "react-router-dom";
+// import {BrowserRouter as Router ,Routes ,Route} from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ChatPage from "./pages/ChatPage";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import { useEffect } from "react";
+// import { useDispatch,useSelector } from "react-redux";
+// import { getUser } from "./store/slices/authSlice";
+// import { connectSocket } from "./socket/socket";
+
+// function App() {
+
+//   const dispatch = useDispatch();
+//   const { user } = useSelector((state) => state.auth);
+
+//   useEffect(() => {
+//     dispatch(getUser());
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     if (user?._id) {
+//       connectSocket(user._id);
+//     }
+//   }, [user?._id]);
+  
+
+//   return (
+
+//     <Routes>
+//       <Route path="/" element={<Login/>} />
+//       <Route path="/login" element={<Login/>} />
+//       <Route path="/register" element={<Register/>} />
+//       <Route
+//         path="/chat"
+//         element={
+//           <ProtectedRoute>
+//             <ChatPage />
+//           </ProtectedRoute>
+//         }
+//       />
+
+//     </Routes>
+//   )
+// }
+
+// export default App
+
+// App.jsx
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ChatPage from "./pages/ChatPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useEffect } from "react";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "./store/slices/authSlice";
-import { connectSocket } from "./socket/socket";
 
 function App() {
-
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
+    // This triggers the /api/auth/me call
     dispatch(getUser());
   }, [dispatch]);
 
-  useEffect(() => {
-    if (user?._id) {
-      connectSocket(user._id);
-    }
-  }, [user?._id]);
-  
-
   return (
-
     <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
+      {/* If user is already logged in, don't show login/register pages */}
+      <Route path="/" element={isAuthenticated ? <Navigate to="/chat" /> : <Login />} />
+      <Route path="/login" element={isAuthenticated ? <Navigate to="/chat" /> : <Login />} />
+      <Route path="/register" element={isAuthenticated ? <Navigate to="/chat" /> : <Register />} />
+      
       <Route
         path="/chat"
         element={
@@ -38,9 +79,11 @@ function App() {
           </ProtectedRoute>
         }
       />
-
+      
+      {/* Fallback for any other route */}
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
